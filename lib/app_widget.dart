5eac9549a2af5f7ea6b6feb5a306c96b327ad221
Rafael Rodrigo <rafael.rodrigo.p.a@gmail.com>
2021-06-26 00:24:44 -0300

    Dia 4 completo

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,23 +1,38 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:payflow/modules/barcode_scanner/barcode_scenner_page.dart';
 import 'package:payflow/modules/home/home_page.dart';
+import 'package:payflow/modules/insert_boleto/insert_boleto_page.dart';
 import 'package:payflow/modules/splash/splash_page.dart';
 import 'modules/login/login_page.dart';
 import 'shared/themes/app_colors.dart';
 
 class AppWidget extends StatelessWidget {
+  AppWidget() {
+    SystemChrome.setPreferredOrientations(
+
+      [
+        DeviceOrientation.portraitDown,
+        DeviceOrientation.portraitUp,
+      ]
+
+    );
+  }
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Pay Flow',
-      theme: ThemeData(primaryColor: AppColors.primary),
-      initialRoute: "/splash",
-      routes: {
-        "/splash": (context) => SplashPage(),
-        "/home": (context) => HomePage(),
-        "/login": (context) => LoginPage()
-      }
-    );
+        title: 'Pay Flow',
+        theme: ThemeData(
+            primaryColor: AppColors.primary, primarySwatch: Colors.orange),
+        initialRoute: "/splash",
+        routes: {
+          "/splash": (context) => SplashPage(),
+          "/home": (context) => HomePage(),
+          "/login": (context) => LoginPage(),
+          "/barcode_scanner": (context) => BarcodeScannerPage(),
+          "/insert_boleto": (context) => InsertBoletoPage()
+        });
   }
 }
